378ab34e731d89c111326a93f7140815d99cf080
Rajasekar <rs20jan1998s@gmail.com>
2023-01-02 13:02:15 +0530

    init

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -7,22 +7,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText msginput;
-    ImageButton sendButton;
-    ListView listView;
-    ArrayList sentmessages;
-    public  static ArrayList receiver;
-    public static String message;
-    public static int sendOrReceive;
-    MessageAdapter adapter;
-    public static String myString;
-    public static ArrayList<AllMessages> allmessages;
+    ListView myListview;
+    EditText edittext;
+    ImageButton sendbutton;
 
 
 
@@ -31,48 +25,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        allmessages=new ArrayList<>();
-        sentmessages=new ArrayList<>();
-        receiver=new ArrayList<>();
-        try{
-            InputStream iStream = getAssets().open("aadhi.json");
-            int size = iStream.available();
-            byte[] buffer = new byte[size];
-            iStream.read(buffer);
-            iStream.close();
-            myString = new String(buffer, "UTF-8");
-            System.out.println(myString);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        sendbutton=findViewById(R.id.sendbutton);
+        edittext=findViewById(R.id.edittext);
+        myListview=findViewById(R.id.mylistview);
 
-        msginput=findViewById(R.id.edittext);
-        sendButton=findViewById(R.id.sendbutton);
-        listView=findViewById(R.id.mylistview);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        sendbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendOrReceive=1;
-                message=msginput.getText().toString();
-                sentmessages.add(message);
-                allmessages.add(new AllMessages(message,0));
-                System.out.println(sentmessages.size());
-                msginput.setText("");
-
-                adapter=new MessageAdapter(MainActivity.this,2,allmessages);
-                listView.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
+                if(edittext.length()!=0){
+                    sendMessage();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Enter the Message first", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-
-
     }
 
-    public static void pushreceivedmessage(String received_message, int i) {
-        System.out.println(received_message+ i);
-        allmessages.add(new AllMessages(received_message,i));
+    private void sendMessage() {
+
     }
 
 }
